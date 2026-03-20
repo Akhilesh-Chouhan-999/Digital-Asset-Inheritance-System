a62e1dbfd7a89090cda597303783a7c84b8e4a0c
Akhilesh Chouhan <akhilesh.chouhan@example.com>
2026-03-20 20:55:01 +0530

    chore: reorganize folder structure - move all folders into src/

--- server/app.js
+++ server/app.js
@@ -1,3 +1,22 @@
-import express from 'express' ; 
+import app from "./app.js";
+import connectDB from "./config/db.config.js";
+import { PORT } from "./config/env.js";
 
-const app = express() ; 
+
+const startServer = async () => {
+
+  try {
+
+    connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server is listening on PORT ${PORT}`);
+    });
+
+  } catch (error) {
+    console.log('Server Startup Failed ');
+    process.exit();
+  }
+}
+
+startServer();
